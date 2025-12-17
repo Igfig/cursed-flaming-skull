48ef1e38198fb454f5779a280d638506d6289d8c
ifich <ira.fich@gmail.com>
2025-12-16 23:39:03 -0500

    Rework latest posts lists to include excerpts, and style appropriately

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "Cursed Flaming Skull",
 	url: "https://cursedflamingskull.com",
 	language: "en",
-	description: "Born to be a flaming skeleton, cursed to be a flaming skull",
+	description: "Born to be a flaming skeleton, cursed to be a flaming skull. OSR and game design blog.",
 	author: {
 		name: "Ira Fich",
 		url: "https://irafich.com"
